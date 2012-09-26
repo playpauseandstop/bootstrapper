ccfcc12f204a6a571810f68b44013e93f9dbfc6f
Igor Davydenko <playpauseandstop@gmail.com>
2012-09-26 17:03:40 +0300

    Update getting version for bootstrapper from setup.py, fix argparse import.

--- setup.py
+++ setup.py
@@ -1,28 +1,25 @@
 #!/usr/bin/env python
 
+import imp
 import os
 import sys
-import subprocess
-import tempfile
 
 from distutils.core import setup
 
 
 DIRNAME = os.path.abspath(os.path.dirname(__file__))
+rel = lambda *parts: os.path.abspath(os.path.join(DIRNAME, *parts))
+
 kwargs = \
     {'install_requires': ['argparse']} if sys.version_info < (2, 7) else {}
 
-handler = open(os.path.join(DIRNAME, 'README.rst'))
+handler = open(rel('README.rst'))
 README = handler.read()
 handler.close()
 
-handler = tempfile.TemporaryFile()
-code = subprocess.check_call('{0}/bootstrapper --version'.format(DIRNAME),
-                             shell=True,
-                             stderr=handler)
-handler.seek(0)
-version = handler.read().strip()
-handler.close()
+module = imp.load_source('bootstrapper', rel('bootstrapper'))
+version = module.__version__
+os.unlink(rel('bootstrapperc'))
 
 setup(
     name='bootstrapper',
